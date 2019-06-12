006cbab59be3630a058a4ca639f7d9ccc47720d5
Maxim Suhinin <suhinin.maxim@gmail.com>
2019-06-12 08:32:06 +0300

    Creation

---
+++ GamesCatalog/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GamesCatalog
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
